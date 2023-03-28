c85a41db6a239a0f117c2a7123fbf1650032e318
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-28 12:07:12 +0800

    Init BreadCrumb class

---
+++ lib/screens/home/classes/bread_crumb.dart
@@ -0,0 +1,24 @@
+import 'package:uuid/uuid.dart';
+
+class BreadCrumb {
+
+  final String _name;
+  bool _isActive;
+  String uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  set isActive(bool value) => _isActive = value;
+
+  String get title => '$_name ${_isActive ? '>' : ''} ';
+
+  BreadCrumb({required String name, required bool isActive,}) :
+    _name = name,
+    _isActive = isActive,
+    uuid = const Uuid().v4();
+
+  @override
+  bool operator ==(covariant BreadCrumb other) =>
+      _isActive == other._isActive && _name == other._name;
+}
