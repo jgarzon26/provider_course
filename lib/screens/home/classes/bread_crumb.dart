5610834893126b7facb6704ddd9afd7ddda78261
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-30 10:58:32 +0800

    CreatingBreadCrumbWidget and Rooting its Provider

--- lib/screens/home/classes/bread_crumb.dart
+++ lib/screens/home/classes/bread_crumb.dart
@@ -3,22 +3,19 @@ import 'package:uuid/uuid.dart';
 class BreadCrumb {
 
   final String _name;
-  bool _isActive;
+  bool isActive;
   String uuid;
 
   @override
   int get hashCode => uuid.hashCode;
 
-  set isActive(bool value) => _isActive = value;
+  String get title => '$_name ${isActive ? '>' : ''} ';
 
-  String get title => '$_name ${_isActive ? '>' : ''} ';
-
-  BreadCrumb({required String name, bool isActive = false,}) :
+  BreadCrumb({required String name, this.isActive = false,}) :
     _name = name,
-    _isActive = isActive,
     uuid = const Uuid().v4();
 
   @override
   bool operator ==(covariant BreadCrumb other) =>
-      _isActive == other._isActive && _name == other._name;
+      isActive == other.isActive && _name == other._name;
 }
