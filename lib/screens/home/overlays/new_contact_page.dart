971af9fac1bb3e907adc0ae5de753c3f730d83e0
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-04-01 14:19:31 +0800

    Init newcontactpage

---
+++ lib/screens/home/overlays/new_contact_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NewContactPage extends PopupRoute{
+  @override
+  Color? get barrierColor => Colors.black.withAlpha(0x50);
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  String? get barrierLabel => "Input a New Contact";
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+    return Center();
+  }
+}
