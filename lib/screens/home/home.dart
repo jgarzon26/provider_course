9bc6ba545a5810515b8eed7dd7257e9a9497ff6b
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-04-02 14:26:45 +0800

    AddingConsumerandNavigation

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_course/screens/home/overlays/new_contact_page.dart';
+import 'package:provider_course/screens/home/widgets/bread_crumbs.dart';
+
+import '../../providers/bread_crumb_provider.dart';
 
 class Home extends StatelessWidget{
 
@@ -10,6 +15,29 @@ class Home extends StatelessWidget{
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Consumer<BreadCrumbProvider>(
+            builder: (context, breadCrumbProvider, child) {
+              return BreadCrumbs(
+                breadCrumbs: breadCrumbProvider.breadCrumbs,
+              );
+            },
+          ),
+          TextButton(
+            onPressed: (){
+              Navigator.of(context).push(NewContactPage());
+            },
+            child: const Text('Add new bread crumb'),
+          ),
+          TextButton(
+            onPressed: (){
+              context.read<BreadCrumbProvider>().resetAll();
+            },
+            child: const Text('Reset'),
+          ),
+        ],
       ),
     );
   }
